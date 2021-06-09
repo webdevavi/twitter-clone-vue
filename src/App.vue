af0232d78f48db9bd2cef054ff68ada1223be9b5
devavi-labs <devavilabs@gmail.com>
2021-06-09 23:50:57 +0530

    add: basic func

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="root">
+    @{{ user.username }} - {{ fullName }}
+    <strong>Followers - {{ followers }}</strong>
+    <button @click="followUser">Follow</button>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from "./components/HelloWorld.vue"
-
   export default {
     name: "App",
-    components: {
-      HelloWorld
+    data: () => ({
+      followers: 0,
+      user: {
+        id: 1,
+        username: "webdevavi",
+        firstName: "Avinash",
+        lastName: "Sinha",
+        email: "savinash2608@gmail.com",
+        isAdmin: true
+      }
+    }),
+    watch: {
+      followers(newFollowersCount, oldFollowersCount) {
+        console.log(`Followers changed from ${oldFollowersCount} to ${newFollowersCount}`)
+      }
+    },
+    computed: {
+      fullName() {
+        return `${this.user.firstName} ${this.user.lastName}`
+      }
+    },
+    methods: {
+      followUser() {
+        this.followers++
+      }
+    },
+    mounted() {
+      this.followUser()
     }
   }
 </script>
 
 <style>
-  #app {
+  #root {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
   }
 </style>
